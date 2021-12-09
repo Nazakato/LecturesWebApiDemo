947fcffa6a19d9eba8528ffe987215c6f6d94f08
Nazakato <kuprian2@i.ua>
2021-12-09 16:19:15 +0200

    Add custom routing

--- WebApiDemo/WebApiDemo/Startup.cs
+++ WebApiDemo/WebApiDemo/Startup.cs
@@ -3,6 +3,7 @@ using Demo.Dal.Books;
 using Demo.Dal.InMemory;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -53,6 +54,10 @@ namespace WebApiDemo
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Welcome to our great API!");
+                });
             });
         }
     }
